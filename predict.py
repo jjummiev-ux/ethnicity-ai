e6778c563ed2673e370dda8d66b658c89ac3bcc8
jjummiev-ux <jjummiev@gmail.com>
2026-04-01 19:15:04 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,10 +1,21 @@
 from fastapi import UploadFile, File
+
+def predict_image(file_bytes):
+    return "OK_TEST"
+
 from fastapi import APIRouter
 
 router = APIRouter()
 
 @router.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    contents = await file.read()
-    
-    return {"class": "test_ok"}
+    try:
+        contents = await file.read()
+        
+        # просто тест
+        result = predict_image(contents)
+
+        return {"result": result}
+
+    except Exception as e:
+        return {"error": str(e)}
